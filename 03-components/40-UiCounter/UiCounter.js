e133f960551f665ee02f7d6cab15a7366ff84739
Pavel <sam0kat4ik@mail.ru>
2024-12-27 00:59:20 +0500

    Компонент-счётчик

--- 03-components/40-UiCounter/UiCounter.js
+++ 03-components/40-UiCounter/UiCounter.js
@@ -9,15 +9,45 @@ export default defineComponent({
     UiButton,
   },
 
-  setup() {
+  props: {
+    min: {
+      type: Number,
+      default: 0,
+    },
+
+    max: {
+      type: Number,
+      default: Infinity,
+    },
+
+    count: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  emits: ['update:count'],
+
+  setup(props, { emit }) {
+    function counter(sign) {
+      if (sign === '+') {
+        emit('update:count', props.count + 1)
+      } else {
+        emit('update:count', props.count - 1)
+      }
+    }
+
+    return {
+      counter,
+    }
     // Рекомендуется для практики реализовать обработку событий внутри setup, а не непосредственно в шаблоне
   },
 
   template: `
     <div class="counter">
-      <UiButton aria-label="Decrement" disabled>➖</UiButton>
-      <span class="count" data-testid="count">3</span>
-      <UiButton aria-label="Increment">➕</UiButton>
+      <UiButton @click="counter('-')" aria-label="Decrement" :disabled="min === count">➖</UiButton>
+      <span class="count" data-testid="count">{{ count }}</span>
+      <UiButton @click="counter('+')" aria-label="Increment" :disabled="max === count">➕</UiButton>
     </div>
   `,
 })
